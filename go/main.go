74c7ed551ead8500dab1e3f03493c5f3bb8ba0a6
duyle <duylk@tenxtenx.com>
2023-10-03 22:09:49 +0700

    update kong, vault, vso

---
+++ go/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+       _, _ = fmt.Fprintf(w, "Simple Server")
+    })
+
+    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+       _, _ = fmt.Fprintf(w, "Hi Simple Server")
+    })
+
+    log.Fatal(http.ListenAndServe(":80", nil))
+}
