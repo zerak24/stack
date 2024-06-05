b0ef62d7113d977f6c68e51ee5322f5900843ed4
duyle <duylk@tenxtenx.com>
2024-06-05 10:23:23 +0700

    update

--- cloud/gcp/modules/nat/main.tf
+++ cloud/gcp/modules/nat/main.tf
@@ -3,17 +3,17 @@ module "nat" {
     for idx, object in var.inputs :
     idx => object
   }
-  source                             = "git@github.com:zerak24/terraform_modules.git//gcp/nat"
-  project_id                         = var.project.project_id
-  region                             = var.project.region
-  name                               = format("%v-%v", var.project.network_name, each.value.name)
-  create_router                      = each.value.create_router
-  router                             = format("%v-%v-router", var.project.network_name, each.value.name)
-  network                            = var.project.network_name
-  subnetworks                        = [for sub in each.value.subnets : 
+  source        = "git@github.com:zerak24/terraform_modules.git//gcp/nat"
+  project_id    = var.project.project_id
+  region        = var.project.region
+  name          = format("%v-%v", var.project.network_name, each.value.name)
+  create_router = each.value.create_router
+  router        = format("%v-%v-router", var.project.network_name, each.value.name)
+  network       = var.project.network_name
+  subnetworks = [for sub in each.value.subnets :
     {
-      name = format("%v-%v", var.project.network_name, each.value.name)
-      source_ip_ranges_to_nat = sub.source_ip_ranges_to_nat
+      name                     = format("%v-%v", var.project.network_name, each.value.name)
+      source_ip_ranges_to_nat  = sub.source_ip_ranges_to_nat
       secondary_ip_range_names = sub.secondary_ip_range_names
     }
   ]
