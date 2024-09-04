e265ef96d0a39eff4f48e8a7097d28557023d3e5
duyle <duylk@tenxtenx.com>
2024-09-04 18:42:27 +0700

    update

--- cloud/aws/modules/core/main.tf
+++ cloud/aws/modules/core/main.tf
@@ -117,6 +117,7 @@ module "eks" {
     eks-pod-identity-agent = {}
     kube-proxy             = {}
     vpc-cni                = {}
+    aws-ebs-csi-driver     = {}
   }
 
   vpc_id                   = module.vpc[0].vpc_id
